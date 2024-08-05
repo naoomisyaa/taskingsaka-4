3f2f5280edf9453aa83019a0608ca8d6dee610c5
rvldrh <rivaldo0347t@gmail.com>
2024-08-05 14:35:19 +0700

    crud

--- src/redux/slices/postSlice.ts
+++ src/redux/slices/postSlice.ts
@@ -14,6 +14,18 @@ const postsSlice = createSlice({
       const { id, title, description } = action.payload;
       state.push({ id, title, description });
     },
+    updatePost: (state, action: PayloadAction<any>) => {
+      const { id, title, description } = action.payload;
+      const postIndex = state.findIndex((post: any) => post.id === id);
+      if (postIndex !== -1) {
+        state[postIndex].title = title;
+        state[postIndex].description = description;
+      }
+    },
+    deletePost: (state, action: PayloadAction<any>) => {
+      const postId = action.payload;
+      return state.filter((post: any) => post.id !== postId);
+    },
   },
 });
 
